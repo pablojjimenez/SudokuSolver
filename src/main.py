760515d9c351ccd34c37cc0746027590250e92ba
pablojj1808 <pablojjinfo@gmail.com>
2021-08-05 18:41:53 +0200

    🔩 testing initialize utils

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from sudoku import Sudoku
-
+import json
 
 s = Sudoku('003020600900305001001806400008102900700000008006708200002609500800203009005010300')
-print(s)
+
+print(json.dumps(s.neighbours))
+#print(s.neighbours)
--- src/sudoku.py
+++ src/sudoku.py
@@ -1,15 +1,46 @@
 from posibles import *
 
 class Sudoku:
+
     def __init__(self, sudoku: str):
+        self.groups     = [[] for i in range(27)]
+        self.groups_of  = {} #<int, (3)>
+        self.neighbours = [[] for i in range(82)]
         self.cells = [Possibles() for i in range(1, 82)]
+        
+        self.initialize_utils()
+        '''
         c = 0
         for i in sudoku:
             if i != '0' and i != '.':
                 self.assign(c,int(i)) 
             c += 1
+        '''
         
-
+    def initialize_utils(self):
+        """
+            The board is a matrix 9 x 9
+            g the position of the index depending on the 
+            group I'm in. Remember 3 roups: rows, columns 
+            and squares
+        """
+        for i in range(9):
+            for j in range(9):
+                k = i * 9 + j # tranformed into array index
+                g = (i, 9 + j, 18 + int(i / 3)*3 + int(j/3))
+                
+                for w in g:
+                    self.groups[w].append(k)
+                    self.groups_of[k] = g
+        # neighbours
+        print(len(self.groups_of[k]) - 1)
+        for k in range(len(self.neighbours)):
+            for i in range(len(self.groups_of[k]) - 1):
+                for j in range(9):
+                    k2 = self.groups[self.groups_of[k][i]][j]
+                    if k2 != k:
+                        self.neighbours[k].append(k2)
+        
     def is_solved(self) -> bool:
         for i in self.cells:
             if not i.only_one(): return False
